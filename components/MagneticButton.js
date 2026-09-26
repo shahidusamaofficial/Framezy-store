73d7c4e51044114231af5403f0502f7537aa348d
shahidusamaofficial <shahidusama.official@gmail.com>
2026-09-26 09:10:05 +0100

    Add MagneticButton component with motion effects

---
+++ components/MagneticButton.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { motion, useMotionValue, useSpring } from "framer-motion";
+
+export default function MagneticButton({ children }) {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const springX = useSpring(x, { stiffness: 200, damping: 15, mass: 0.4 });
+  const springY = useSpring(y, { stiffness: 200, damping: 15, mass: 0.4 });
+
+  function handleMouseMove(e) {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const offsetX = e.clientX - (rect.left + rect.width / 2);
+    const offsetY = e.clientY - (rect.top + rect.height / 2);
+    x.set(offsetX * 0.3);
+    y.set(offsetY * 0.3);
+  }
+
+  function handleMouseLeave() {
+    x.set(0);
+    y.set(0);
+  }
+
+  return (
+    <motion.div
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ x: springX, y: springY }}
+      className="inline-block"
+    >
+      {children}
+    </motion.div>
+  );
+}
